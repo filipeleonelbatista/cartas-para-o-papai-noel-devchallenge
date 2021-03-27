6169150cee9c7e0844013f03e8738426e554acae
Filipe Batista <filipe.x@live.com>
2021-03-27 12:47:04 -0300

    ajustando server

--- mobile/src/services/api.js
+++
@@ -1,8 +0,0 @@
-import axios from 'axios';
-
-const api = axios.create({
-    // baseURL: 'http://192.168.100.7:3333',
-    baseURL: "https://cartasparaopapainoel.herokuapp.com",
-});
-
-export default api;
