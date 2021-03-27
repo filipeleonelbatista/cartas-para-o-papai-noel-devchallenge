2646b37e165ce3f0c2d9759a27c1350990bd513b
Filipe Batista <filipe.x@live.com>
2021-03-27 12:51:59 -0300

    configurando banco

--- src/database/connection.js
+++ src/database/connection.js
@@ -3,10 +3,10 @@ const knex = require('knex');
 const db = knex({
     client: 'pg',
     connection: {
-        host: 'localhost',
-        user: 'postgres',
-        password: 'docker',
-        database: 'cartas',
+        host: 'ec2-18-211-97-89.compute-1.amazonaws.com',
+        user: 'lcspdyciahadqo',
+        password: 'b7503eefdf0843adff395dd28e8065fcc2a43393a1cb3091a1029ec2f7dd0ac4',
+        database: 'dvdinslidqtvq',
         port: '5432',
         ssl: { rejectUnauthorized: false }
     },
