3ec1596e1261552f92fc3060c9ca05406aacd95a
Filipe Batista <filipe.x@live.com>
2021-03-27 12:37:15 -0300

    publicando aplicação

--- mobile/src/services/api.js
+++ mobile/src/services/api.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'http://192.168.100.7:3333',
+    // baseURL: 'http://192.168.100.7:3333',
+    baseURL: "https://cartasparaopapainoel.herokuapp.com",
 });
 
 export default api;
--- server/knexfile.js
+++ server/knexfile.js
@@ -3,14 +3,16 @@ const path = require('path');
 module.exports = {
     client: 'pg',
     connection: {
-        host : 'localhost',
-        user : 'postgres',
-        password : 'docker',
-        database : 'cartas',
-        port: '5432'
+        host: 'localhost',
+        user: 'postgres',
+        password: 'docker',
+        database: 'cartas',
+        port: '5432',
+        ssl: { rejectUnauthorized: false }
     },
     migrations: {
         directory: path.resolve(__dirname, 'src', 'database', 'migrations')
+        //directory: path.resolve(__dirname, 'dist', 'database', 'migrations')
     },
     useNullAsDefault: true,
 };
