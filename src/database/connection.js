56e21eda1b447566b8a60210ad9c5e81e2ae34e7
Filipe Batista <filipe.x@live.com>
2021-11-24 10:06:57 -0300

    ajuste nova database

--- src/database/connection.js
+++ src/database/connection.js
@@ -3,10 +3,10 @@ const knex = require('knex');
 const db = knex({
     client: 'pg',
     connection: {
-        host: 'ec2-18-211-97-89.compute-1.amazonaws.com',
-        user: 'lcspdyciahadqo',
-        password: 'b7503eefdf0843adff395dd28e8065fcc2a43393a1cb3091a1029ec2f7dd0ac4',
-        database: 'dvdinslidqtvq',
+        host: 'ec2-54-159-126-187.compute-1.amazonaws.com',
+        user: 'fsgofyvaodjuxs',
+        password: 'adb7e21ee1054e8e8a80032d371afdf93829420a19b953074fb5dad2a63af319',
+        database: 'd5p68uf8kkq7pf',
         port: '5432',
         ssl: { rejectUnauthorized: false }
     },
